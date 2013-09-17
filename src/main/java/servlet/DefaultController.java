872bbc325bb9e60fbae9da5389b6a5a3a3361413
ashish343 <singhalashish343@gmail.com>
2013-09-17 15:08:35 +0530

    Adding controller

--- src/main/java/servlet/DefaultController.java
+++ src/main/java/servlet/DefaultController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
