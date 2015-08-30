505a2401b5156645ee1aea1bf35f355a9523692a
Sinha <mohsinha@amazon.com>
2015-08-30 13:33:11 +0530

    null pointer bug

--- src/main/java/com/servlet/ConfigGetController.java
+++ src/main/java/com/servlet/ConfigGetController.java
@@ -9,19 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.elasticsearch.common.lang3.StringUtils;
+
 @SuppressWarnings("serial")
 @WebServlet(
-        name = "ConfigGetController", 
-        urlPatterns = {"/configget"}
-    )
+		name = "ConfigGetController", 
+		urlPatterns = {"/configget"}
+		)
 public class ConfigGetController extends HttpServlet {
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String config = DataInsertionService.findConfig();
-        
-        ServletOutputStream out = response.getOutputStream();
-        out.write(config.getBytes());
-        out.flush();
-    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String config = DataInsertionService.findConfig();
+
+		if(StringUtils.isNoneBlank(config)){
+			ServletOutputStream out = response.getOutputStream();
+			out.write(config.getBytes());
+			out.flush();
+		}
+	}
 }
